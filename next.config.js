af316d3dd0d85ade4611be22026a3bd02ec9c116
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.coom>
2025-05-25 05:03:41 +0530

    Revert "Fix SWC loading issue"
    This reverts commit 64ff891eebdd58976ef878626dab4f37c446f819.

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
         domains: [
             "files.edgestore.dev"
         ]
-    },
-    // Disable SWC and use Babel instead
-    swcMinify: false
+    }
 }
 
 module.exports = nextConfig
